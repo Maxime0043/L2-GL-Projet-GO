4ca59911f6ed4662d4ae7cf63d563074fa3ce45f
Micaël <Mical@192.168.0.20>
2020-02-04 15:12:25 +0100

    score v1

--- GL_Projet_GO/src/data/Score.java
+++ GL_Projet_GO/src/data/Score.java
@@ -5,6 +5,11 @@ public class Score {
 	private int capture;
 	private int territoire;
 	private int komi;
+	
+	public Score (int komi) {
+		this.capture = 0;
+		this.komi = komi;
+	}
 
 	public int total() {
 		int total;
