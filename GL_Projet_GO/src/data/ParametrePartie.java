8f32a23029e5a938d931b1dd936750236771288c
Micaël <Mical@192.168.0.20>
2020-02-04 16:19:27 +0100

    parametrePartie

---
+++ GL_Projet_GO/src/data/ParametrePartie.java
@@ -0,0 +1,11 @@
+package data;
+
+import java.util.HashMap;
+
+public class ParametrePartie {
+	private int nombrebJoueurs;
+	private int nombrebIA;
+	private int difficulteIA;
+	private int tailleGoban;
+	private HashMap <String, Integer> hmHandicap; 
+}
