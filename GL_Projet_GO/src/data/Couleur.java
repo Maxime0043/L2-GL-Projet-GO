b84f75509b9955d4368b13b9d5183da15db9a239
Maxime0043 <max.pinna@hotmail.fr>
2020-02-04 15:32:41 +0100

    Couleur

---
+++ GL_Projet_GO/src/data/Couleur.java
@@ -0,0 +1,17 @@
+package data;
+
+public enum Couleur {
+	NOIR(0),
+	BLANC(1),
+	ROUGE(2);
+	
+	private int couleur;
+	
+	Couleur(int couleur){
+		this.couleur = couleur;
+	}
+	
+	public int getCouleur() {
+		return couleur;
+	}
+}
