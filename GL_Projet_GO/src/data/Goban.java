a0096662288ec97fb1c7bda578456bc7a176061f
Micaël <Mical@192.168.0.20>
2020-02-04 15:51:37 +0100

    goban

--- GL_Projet_GO/src/data/Goban.java
+++ GL_Projet_GO/src/data/Goban.java
@@ -8,7 +8,16 @@ public class Goban {
 	private HashMap <String, Chaine> hmChaine;
 	private HashMap <String, Score> scores;
 	
-	public boolean existPierre (Coordonnee coord) {
+	public Goban(int taille) {
+		plateau = new AbstractPierre[taille][taille];
+		hmChaine = new HashMap <String, Chaine>();
+		scores = new HashMap <String, Score>();
+	}
 	
+	public boolean existPierre (Coordonnee coord) {
+		if (plateau[coord.getX()][coord.getY()] != null) {
+			return true;
+		}
+		else return false;
 	}
 }
