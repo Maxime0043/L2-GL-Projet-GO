6500974311f736ef067c8302dbceb54c2b5f9cc9
Maxime0043 <max.pinna@hotmail.fr>
2020-02-04 15:53:52 +0100

    MP

---
+++ GL_Projet_GO/src/data/MegaPierre.java
@@ -0,0 +1,22 @@
+package data;
+
+public class MegaPierre extends AbstractPierre{
+
+	private Coordonnee[] coord;
+	
+	public MegaPierre(Couleur couleur, int liberte, String nomChaine, Coordonnee[] coord) {
+		super(couleur, liberte, nomChaine);
+		this.coord = coord;
+	}
+
+	@Override
+	public boolean voisin(AbstractPierre[][] plateau) {
+		return false;
+	}
+
+	@Override
+	public boolean vivante() {
+		return true;
+	}
+
+}
