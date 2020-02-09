8a8c654f80a7e28bc3ae5a516e63e217049108e9
Maxime0043 <max.pinna@hotmail.fr>
2020-02-09 15:00:00 +0100

    Update Capture - MegaPierre

--- GL_Projet_GO/src/traitement/Capture.java
+++ GL_Projet_GO/src/traitement/Capture.java
@@ -21,21 +21,125 @@ public class Capture {
 		int x = pierre.getX();
 		int y = pierre.getY();
 		
-		AbstractPierre haut = plateau[x-1][y];
-		AbstractPierre bas = plateau[x+1][y];
-		AbstractPierre gauche = plateau[x][y-1];
-		AbstractPierre droite = plateau[x][y+1];
-		
-		String couleurHaut = haut.getCouleur();
-		String couleurBas = bas.getCouleur();
-		String couleurGauche = gauche.getCouleur();
-		String couleurDroite = droite.getCouleur();
-		
 		if(pierre.isMegaPierre()) {
+			AbstractPierre haut1 = plateau[x-1][y];
+			AbstractPierre haut2 = plateau[x-1][y+1];
+			AbstractPierre bas1 = plateau[x+2][y];
+			AbstractPierre bas2 = plateau[x+2][y+1];
+			AbstractPierre gauche1 = plateau[x][y-1];
+			AbstractPierre gauche2 = plateau[x+1][y-1];
+			AbstractPierre droite1 = plateau[x][y+2];
+			AbstractPierre droite2 = plateau[x-1][y+2];
 			
+			String couleurHaut1 = haut1.getCouleur();
+			String couleurHaut2 = haut2.getCouleur();
+			String couleurBas1 = bas1.getCouleur();
+			String couleurBas2 = bas2.getCouleur();
+			String couleurGauche1 = gauche1.getCouleur();
+			String couleurGauche2 = gauche2.getCouleur();
+			String couleurDroite1 = droite1.getCouleur();
+			String couleurDroite2 = droite2.getCouleur();
+			
+			if(gopierre.bordHaut(pierre)) {
+				if(gopierre.bordGauche(pierre)) {
+					if(couleurPierre.equals(couleurBas1) ||couleurPierre.equals(couleurBas2) ||  couleurPierre.equals(couleurDroite1) || couleurPierre.equals(couleurDroite2)) {
+						return false;
+					}
+					if(couleurBas1.equals(couleurBas2) && couleurBas1.equals(couleurDroite1) && couleurBas1.equals(couleurDroite2)) {
+						return true;
+					}
+				}
+				else if(gopierre.bordDroit(pierre, choix)) {
+					if(couleurPierre.equals(couleurBas1) ||couleurPierre.equals(couleurBas2) ||  couleurPierre.equals(couleurGauche1) || couleurPierre.equals(couleurGauche2)) {
+						return false;
+					}
+					if(couleurBas1.equals(couleurBas2) && couleurBas1.equals(couleurGauche1) && couleurBas1.equals(couleurGauche2)) {
+						return true;
+					}
+				}
+				else {
+					if(couleurPierre.equals(couleurBas1) ||couleurPierre.equals(couleurBas2) ||  couleurPierre.equals(couleurGauche1) || 
+							couleurPierre.equals(couleurGauche2) ||  couleurPierre.equals(couleurDroite1) || couleurPierre.equals(couleurDroite2)) {
+						return false;
+					}
+					if(couleurBas1.equals(couleurBas2) && couleurBas1.equals(couleurGauche1) && couleurBas1.equals(couleurGauche2) && couleurBas1.equals(couleurDroite1) && couleurBas1.equals(couleurDroite2)) {
+						return true;
+					}
+				}
+			}
+			
+			else if(gopierre.bordBas(pierre, choix)) {
+				if(gopierre.bordGauche(pierre)) {
+					if(couleurPierre.equals(couleurHaut1) ||couleurPierre.equals(couleurHaut2) ||  couleurPierre.equals(couleurDroite1) || couleurPierre.equals(couleurDroite2)) {
+						return false;
+					}
+					if(couleurHaut1.equals(couleurHaut2) && couleurHaut1.equals(couleurDroite1) && couleurHaut1.equals(couleurDroite2)) {
+						return true;
+					}
+				}
+				else if(gopierre.bordDroit(pierre, choix)) {
+					if(couleurPierre.equals(couleurHaut1) ||couleurPierre.equals(couleurHaut2) ||  couleurPierre.equals(couleurGauche1) || couleurPierre.equals(couleurGauche2)) {
+						return false;
+					}
+					if(couleurHaut1.equals(couleurHaut2) && couleurHaut1.equals(couleurGauche1) && couleurHaut1.equals(couleurGauche2)) {
+						return true;
+					}
+				}
+				else {
+					if(couleurPierre.equals(couleurHaut1) ||couleurPierre.equals(couleurHaut2) ||  couleurPierre.equals(couleurGauche1) || 
+							couleurPierre.equals(couleurGauche2) ||  couleurPierre.equals(couleurDroite1) || couleurPierre.equals(couleurDroite2)) {
+						return false;
+					}
+					if(couleurHaut1.equals(couleurHaut2) && couleurHaut1.equals(couleurGauche1) && couleurHaut1.equals(couleurGauche2) && couleurHaut1.equals(couleurDroite1) && couleurHaut1.equals(couleurDroite2)) {
+						return true;
+					}
+				}	
+			}
+			
+			else if(gopierre.bordGauche(pierre)) {
+				if(couleurPierre.equals(couleurHaut1) ||couleurPierre.equals(couleurHaut2) ||  couleurPierre.equals(couleurBas1) || 
+						couleurPierre.equals(couleurBas2) ||  couleurPierre.equals(couleurDroite1) || couleurPierre.equals(couleurDroite2)) {
+					return false;
+				}
+				if(couleurHaut1.equals(couleurHaut2) && couleurHaut1.equals(couleurBas1) && couleurHaut1.equals(couleurBas2) && couleurHaut1.equals(couleurDroite1) && couleurHaut1.equals(couleurDroite2)) {
+					return true;
+				}
+			}
+			
+			else if(gopierre.bordDroit(pierre, choix)) {
+				if(couleurPierre.equals(couleurHaut1) ||couleurPierre.equals(couleurHaut2) ||  couleurPierre.equals(couleurBas1) || 
+						couleurPierre.equals(couleurBas2) ||  couleurPierre.equals(couleurGauche1) || couleurPierre.equals(couleurGauche2)) {
+					return false;
+				}
+				if(couleurHaut1.equals(couleurHaut2) && couleurHaut1.equals(couleurBas1) && couleurHaut1.equals(couleurBas2) && couleurHaut1.equals(couleurGauche1) && couleurHaut1.equals(couleurGauche2)) {
+					return true;
+				}
+			}
+			
+			else {
+				if(couleurPierre.equals(couleurHaut1) ||couleurPierre.equals(couleurHaut2) ||  couleurPierre.equals(couleurBas1) || couleurPierre.equals(couleurBas2) ||  
+						couleurPierre.equals(couleurGauche1) || couleurPierre.equals(couleurGauche2) || couleurPierre.equals(couleurDroite1) || couleurPierre.equals(couleurDroite2)) {
+					return false;
+				}
+				if(couleurHaut1.equals(couleurHaut2) && couleurHaut1.equals(couleurBas1) && couleurHaut1.equals(couleurBas2) && couleurHaut1.equals(couleurGauche1) && 
+						couleurHaut1.equals(couleurGauche2) && couleurHaut1.equals(couleurDroite1) && couleurHaut1.equals(couleurDroite2)) {
+					return true;
+				}
+			}
 		}
 		
 		else {
+			AbstractPierre haut = plateau[x-1][y];
+			AbstractPierre bas = plateau[x+1][y];
+			AbstractPierre gauche = plateau[x][y-1];
+			AbstractPierre droite = plateau[x][y+1];
+			
+			String couleurHaut = haut.getCouleur();
+			String couleurBas = bas.getCouleur();
+			String couleurGauche = gauche.getCouleur();
+			String couleurDroite = droite.getCouleur();
+			
+			
 			if(gopierre.bordHaut(pierre)) {
 				if(gopierre.bordGauche(pierre)) {
 					if(couleurPierre.equals(couleurBas) || couleurPierre.equals(couleurDroite)) {
