1744dbfb7e7b1070c93260ddc7864581583ed880
Maxime0043 <max.pinna@hotmail.fr>
2020-02-10 22:37:19 +0100

    Update Liberte (FINI !!!)

--- GL_Projet_GO/src/traitement/Liberte.java
+++ GL_Projet_GO/src/traitement/Liberte.java
@@ -1,7 +1,5 @@
 package traitement;
 
-import java.util.ArrayList;
-
 import donnees.AbstractPierre;
 
 /**
@@ -14,12 +12,12 @@ public class Liberte {
 	private int nb_liberte;
 	private AbstractPierre pierre;
 	private GoPierre gopierre;
-	private ArrayList<AbstractPierre> pierresAutour;
-	
-	private boolean isBordHaut = false;
-	private boolean isBordBas = false;
-	private boolean isBordGauche = false;
-	private boolean isBordDroit = false;
+//	private ArrayList<AbstractPierre> pierresAutour;
+//	
+//	private boolean isBordHaut = false;
+//	private boolean isBordBas = false;
+//	private boolean isBordGauche = false;
+//	private boolean isBordDroit = false;
 	
 	/**
 	 * 
@@ -28,13 +26,15 @@ public class Liberte {
 	public Liberte(AbstractPierre pierre) {
 		this.pierre = pierre;
 		
-		if(pierre.isMegaPierre())
-			nb_liberte = 8;
-		else
-			nb_liberte = 4;
+//		if(pierre.isMegaPierre())
+//			nb_liberte = 8;
+//		else
+//			nb_liberte = 4;
+		
+		nb_liberte = 0;
 		
 		gopierre = new GoPierre();
-		pierresAutour = new ArrayList<AbstractPierre>();
+//		pierresAutour = new ArrayList<AbstractPierre>();
 	}
 
 	/**
@@ -51,58 +51,95 @@ public class Liberte {
 	 * @param choix
 	 */
 	public void updateLiberte(AbstractPierre[][] plateau, int choix) {
-		ArrayList<AbstractPierre> voisins = gopierre.voisins(pierre, plateau, choix);
+		nb_liberte = 0;
 		
-		if(voisins.size() > 0) {
-			if(pierresAutour.size() == 0) {
-				pierresAutour.addAll(voisins);
-				nb_liberte -= voisins.size();
-			}
-			else {
-				ArrayList<AbstractPierre> tmp = new ArrayList<AbstractPierre>();
-				tmp.addAll(pierresAutour);
-				
-				int x1, x2, y1, y2;
-			
-				for(AbstractPierre p1 : voisins) {
-					x1 = p1.getX();
-					y1 = p1.getY();
-					
-					for(AbstractPierre p2 : tmp) {
-						x2 = p2.getX();
-						y2 = p2.getY();
-						
-						if((x1 != x2) && (y1 != y2)) {
-							pierresAutour.add(p1);
-							nb_liberte--;
-						}
-					}
-				}
-				System.out.println("Voisins : " + pierresAutour.size());
-			}
-		}
-			
-		if(pierre.isMegaPierre()) {
-			
+		if(!gopierre.bordHaut(pierre)) {
+			nb_liberte++;
 		}
 		
-		else {
-			if(gopierre.bordHaut(pierre) && !isBordHaut) {
-				nb_liberte--;
-				isBordHaut = true;
-			}
-			if(gopierre.bordBas(pierre, choix) && !isBordBas) {
-				nb_liberte--;
-				isBordBas = true;
-			}
-			if(gopierre.bordGauche(pierre) && !isBordGauche) {
-				nb_liberte--;
-				isBordGauche = true;
-			}
-			if(gopierre.bordDroit(pierre, choix) && !isBordDroit) {
-				nb_liberte--;
-				isBordDroit = true;
-			}
+		if(!gopierre.bordBas(pierre, choix)) {
+			nb_liberte++;
 		}
+		
+		if(!gopierre.bordGauche(pierre)) {
+			nb_liberte++;
+		}
+		
+		if(!gopierre.bordDroit(pierre, choix)) {
+			nb_liberte++;
+		}
+		
+		nb_liberte -= gopierre.voisins(pierre, plateau, choix).size();
+		
+//		ArrayList<AbstractPierre> voisins = gopierre.voisins(pierre, plateau, choix);
+//		
+//		if(voisins.size() > 0) {
+//			if(pierresAutour.size() == 0) {
+//				pierresAutour.addAll(voisins);
+//				nb_liberte -= voisins.size();
+//			}
+//			else {
+//				ArrayList<AbstractPierre> tmp = new ArrayList<AbstractPierre>();
+//				tmp.addAll(pierresAutour);
+//				pierresAutour.clear();
+//				
+//				int x1, x2, y1, y2;
+//			
+//				for(AbstractPierre p1 : voisins) {
+//					x1 = p1.getX();
+//					y1 = p1.getY();
+//					
+//					for(AbstractPierre p2 : tmp) {
+//						x2 = p2.getX();
+//						y2 = p2.getY();
+//						
+//						if((x1 != x2) && (y1 != y2)) {
+//							pierresAutour.add(p1);
+//							nb_liberte--;
+//							break;
+//						}
+//					}
+//				}
+//				
+//				for(AbstractPierre p1 : tmp) {
+//					x1 = p1.getX();
+//					y1 = p1.getY();
+//					
+//					for(AbstractPierre p2 : pierresAutour) {
+//						x2 = p2.getX();
+//						y2 = p2.getY();
+//						
+//						if((x1 != x2) && (y1 != y2)) {
+//							nb_liberte++;
+//							break;
+//						}
+//					}
+//				}
+//				System.out.println("Voisins : " + pierresAutour.size());
+//			}
+//		}
+//			
+//		if(pierre.isMegaPierre()) {
+//			
+//		}
+//		
+//		else {
+//			if(gopierre.bordHaut(pierre) && !isBordHaut) {
+//				nb_liberte--;
+//				isBordHaut = true;
+//			}
+//			if(gopierre.bordBas(pierre, choix) && !isBordBas) {
+//				nb_liberte--;
+//				isBordBas = true;
+//			}
+//			if(gopierre.bordGauche(pierre) && !isBordGauche) {
+//				nb_liberte--;
+//				isBordGauche = true;
+//			}
+//			if(gopierre.bordDroit(pierre, choix) && !isBordDroit) {
+//				nb_liberte--;
+//				isBordDroit = true;
+//			}
+//		}
 	}
 }
