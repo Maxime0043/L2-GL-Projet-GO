d78ab28a34796d292e05de007fec29a852c75c9d
Maxime0043 <max.pinna@hotmail.fr>
2020-02-11 18:22:55 +0100

    Update Moteur

--- GL_Projet_GO/src/gui/GoPanel.java
+++ GL_Projet_GO/src/gui/GoPanel.java
@@ -7,6 +7,9 @@ import java.awt.event.MouseListener;
 
 import javax.swing.JPanel;
 
+import donnees.Cercle;
+import donnees.Couleur;
+import donnees.ParametrePartie;
 import traitement.Moteur;
 
 public class GoPanel extends JPanel{
@@ -15,8 +18,16 @@ public class GoPanel extends JPanel{
 	
 	Moteur moteur;
 	
+	int cellule;
+	int ecart_window;
+	int taille_goban;
+	
 	public GoPanel() {
 		moteur = new Moteur();
+		
+		cellule = ParametrePartie.LARGEUR_CASE;
+		ecart_window = ParametrePartie.ECART;
+		taille_goban = ParametrePartie.TAILLE_GOBAN[moteur.getChoix()];
 		
 		this.addMouseListener(new Souris());
 		
@@ -27,7 +38,62 @@ public class GoPanel extends JPanel{
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
-		moteur.paint(g);
+		drawGrid(g);
+		drawCercle(g);
+		drawCouleurJoueur(g);
+	}
+	
+	private void drawGrid(Graphics g){
+		g.setColor(Color.BLACK);
+		
+		for(int i = 0 ; i < taille_goban ; i++){
+			g.drawLine(ecart_window, ecart_window + i * cellule, ecart_window + (taille_goban - 1) * cellule, ecart_window + i * cellule);
+			g.drawLine(ecart_window + i * cellule, ecart_window, ecart_window +i * cellule, ecart_window + (taille_goban - 1) * cellule);
+		}
+	}
+	
+	private void drawCercle(Graphics g) {
+		int x, y;
+		
+		for(Cercle c : moteur.getCercleList()) {
+			x = c.getY() * cellule + ecart_window / 2 - ParametrePartie.ERREUR_NON_CONTROLEE;
+			y = c.getX() * cellule + ecart_window / 2 - ParametrePartie.ERREUR_NON_CONTROLEE;
+			
+			setColor(g, c.getCouleur());
+			
+			g.fillOval(x, y, ParametrePartie.TAILLE_CERCLE, ParametrePartie.TAILLE_CERCLE);
+		}
+	}
+	
+	private void drawCouleurJoueur(Graphics g) {
+		if(moteur.getNoir()) {
+			g.setColor(Color.BLACK);
+		}
+		else if(moteur.getBlanc()) {
+			g.setColor(Color.WHITE);
+		}
+		else if(moteur.getRouge()){
+			g.setColor(Color.RED);
+		}
+		
+		int x = ecart_window;
+		int y = taille_goban * cellule + 15;
+		
+		g.fillOval(x, y, 30, 30);
+	}
+	
+	private void setColor(Graphics g, Couleur couleur) {
+		if(couleur.equals(Couleur.NOIR)) {
+			g.setColor(Color.BLACK);
+		}
+		
+		else if(couleur.equals(Couleur.BLANC)) {
+			g.setColor(Color.WHITE);
+		}
+		
+		else if(couleur.equals(Couleur.ROUGE)) {
+			g.setColor(Color.RED);
+		}
 	}
 	
 	
--- GL_Projet_GO/src/traitement/Moteur.java
+++ GL_Projet_GO/src/traitement/Moteur.java
@@ -1,7 +1,5 @@
 package traitement;
 
-import java.awt.Color;
-import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
@@ -20,16 +18,17 @@ public class Moteur {
 	int ecart_window = ParametrePartie.ECART;
 	int taille_goban = ParametrePartie.TAILLE_GOBAN[choix];
 	
-	private ArrayList<Cercle> cercle;
 	Goban goban;
+	
+	private ArrayList<Cercle> cercle;
 	
 	private boolean noir = true;
 	private boolean blanc = false;
 	private boolean rouge = false;
 	
 	public Moteur() {
-		cercle = new ArrayList<Cercle>();
 		goban = new Goban(choix);
+		cercle = new ArrayList<Cercle>();
 	}
 	
 	public int getChoix() {
@@ -40,65 +39,20 @@ public class Moteur {
 		choix = valeur;
 	}
 	
-	public void paint(Graphics g) {
-		drawGrid(g);
-		drawCercle(g);
-		drawCouleurJoueur(g);
+	public boolean getNoir() {
+		return noir;
 	}
 	
-	private void drawGrid(Graphics g){
-		g.setColor(Color.BLACK);
-		
-		for(int i = 0 ; i < taille_goban ; i++){
-			g.drawLine(ecart_window, ecart_window + i * cellule, ecart_window + (taille_goban - 1) * cellule, ecart_window + i * cellule);
-			g.drawLine(ecart_window + i * cellule, ecart_window, ecart_window +i * cellule, ecart_window + (taille_goban - 1) * cellule);
-		}
+	public boolean getBlanc() {
+		return blanc;
 	}
 	
-	private void drawCercle(Graphics g) {
-		g.setColor(Color.black);
-		
-		int x, y;
-		
-		for(Cercle c : cercle) {
-			x = c.getY() * cellule + ecart_window / 2 - ParametrePartie.ERREUR_NON_CONTROLEE;
-			y = c.getX() * cellule + ecart_window / 2 - ParametrePartie.ERREUR_NON_CONTROLEE;
-			
-			setColor(g, c.getCouleur());
-			
-			g.fillOval(x, y, ParametrePartie.TAILLE_CERCLE, ParametrePartie.TAILLE_CERCLE);
-		}
+	public boolean getRouge() {
+		return rouge;
 	}
 	
-	private void drawCouleurJoueur(Graphics g) {
-		if(noir) {
-			g.setColor(Color.BLACK);
-		}
-		else if(blanc) {
-			g.setColor(Color.WHITE);
-		}
-		else {
-			g.setColor(Color.RED);
-		}
-		
-		int x = ecart_window;
-		int y = taille_goban * cellule + 15;
-		
-		g.fillOval(x, y, 30, 30);
-	}
-	
-	private void setColor(Graphics g, Couleur couleur) {
-		if(couleur.equals(Couleur.NOIR)) {
-			g.setColor(Color.BLACK);
-		}
-		
-		else if(couleur.equals(Couleur.BLANC)) {
-			g.setColor(Color.WHITE);
-		}
-		
-		else if(couleur.equals(Couleur.ROUGE)) {
-			g.setColor(Color.RED);
-		}
+	public ArrayList<Cercle> getCercleList(){
+		return cercle;
 	}
 	
 	public Cercle getCercle(int x, int y) {
