0a96c21a703a95dfe1859e91410a9210a47bb3cc
Micaël <Mical@192.168.0.20>
2020-03-21 12:24:19 +0100

    pierre morte : phase 2

--- GL_Projet_GO/src/traitement/FinDePartie.java
+++ GL_Projet_GO/src/traitement/FinDePartie.java
@@ -135,7 +135,8 @@ public class FinDePartie {
 	public void pierreMorte (AbstractPierre[][] plateau, HashMap<Integer, Chaine> hmChaine) {
 		
 		int i, j;
-		boolean ajouter = true;
+
+		boolean marge = false;
 		
 		ArrayList<AbstractPierre> ListPierre = new ArrayList<AbstractPierre>();
 		ArrayList<Integer> ListChaine = new ArrayList<Integer>();
@@ -143,10 +144,9 @@ public class FinDePartie {
 		ArrayList<Coordonnee> finalInterVide = new ArrayList<Coordonnee>();
 		ArrayList<Coordonnee> InterVideDejaParcourue = new ArrayList<Coordonnee>();
 		ArrayList<AbstractPierre> listeInterPleine = new ArrayList<AbstractPierre>();
-		ArrayList<AbstractPierre> InterPleineDejaParcourue = new ArrayList<AbstractPierre>();
 		
-		for(i=0;i<taille_goban-1;i++) {
-			for(j=0;j<taille_goban-1;j++) {
+		for(i=0;i<taille_goban;i++) {
+			for(j=0;j<taille_goban;j++) {
 				if(goban.existPierre(i, j)) {
 					ListPierre.add(goban.getPierre(i, j));
 				}
@@ -173,11 +173,11 @@ public class FinDePartie {
 						}
 						
 						if(finalInterVide.size()>hmChaine.get(pierre.getNomChaine()).getChaine().size()) {
-							vivante = true;
+							marge = true;
 						}
 						
-						listeInterVide.clear();
-						listeInterVide.addAll(finalInterVide);
+						//listeInterVide.clear();
+						//listeInterVide.addAll(finalInterVide);
 						InterVideDejaParcourue.addAll(finalInterVide);
 						
 						while (!vivante && finalInterVide.size() > 0) {				//Récupère les intersections suivantes et verifie si la pierre rencontre les 4 bords
@@ -203,7 +203,7 @@ public class FinDePartie {
 							setVivante();
 						}
 						
-						if(!vivante) {												//Verifie si une chaine alentour existe et est vivante.
+						if(!vivante) {												//Verifie si une chaine alentour existe et est à deux yeux.
 							for(Coordonnee c : InterVideDejaParcourue) {
 								listeInterPleine = GoPierre.voisins(new Pierre(Couleur.NOIR, c), plateau, taille_goban);
 								for(AbstractPierre p : listeInterPleine) {
@@ -215,6 +215,13 @@ public class FinDePartie {
 								}
 							}
 						}
+						if(vivante && !marge) { //si elle a trouvé des alliés mais pas de place pour bouger
+							vivante = false;
+						}
+						if(!vivante && marge) { // si elle est seul mais a de la place pour faire des choses
+							vivante = true;
+						}
+						
 						if(vivante){
 							System.out.println(pierre.getCouleur()  + " / " + pierre.getX() + "," + pierre.getY()+ " : vivante");
 						}
@@ -244,10 +251,79 @@ public class FinDePartie {
 			
 			else {				
 				//la pierre n'appartient pas à une chaine
+				setBord(pierre);
+				finalInterVide = GoPierre.intersectionVide(pierre, plateau, taille_goban);  //recupère les premieres intersections vides autour de la chaine
+				
+//				if(finalInterVide.size()>=2) {
+//					marge = true;
+//				}
+						
+				InterVideDejaParcourue.addAll(finalInterVide);
+						
+				while (!vivante && finalInterVide.size() > 0) {				//Récupère les intersections suivantes et verifie si la pierre rencontre les 4 bords
+					listeInterVide.clear();
+					finalInterVide.clear();
+							
+					for(Coordonnee dc : InterVideDejaParcourue) {
+						setBord(dc);
+						listeInterVide = GoPierre.intersectionVide(new Pierre(Couleur.NOIR, dc), plateau, taille_goban);
+								
+						for(Coordonnee c : listeInterVide) {   
+							if(!dejaParcourue(c, finalInterVide) && !dejaParcourue(c, InterVideDejaParcourue)) {
+								setBord(c);
+								finalInterVide.add(c);
+							}
+						}
+					}
+							
+					for(Coordonnee fc : finalInterVide) {
+						InterVideDejaParcourue.add(fc);
+					}
+
+					setVivante();
+				}
+						
+				if(!vivante) {												//Verifie si une chaine alentour existe et est à deux yeux.
+					for(Coordonnee c : InterVideDejaParcourue) {
+						listeInterPleine = GoPierre.voisins(new Pierre(Couleur.NOIR, c), plateau, taille_goban);
+						for(AbstractPierre p : listeInterPleine) {
+							if(p.hasChaine()) {
+								if(p.getCouleur() == pierre.getCouleur() && p.getNomChaine() != pierre.getNomChaine() && hmChaine.get(p.getNomChaine()).getTwoEyes()) {
+									vivante = true;
+								}
+							}
+						}
+					}
+				}
+				
+//				if(vivante && !marge) { //si elle a trouvé des alliés mais pas de place pour bouger
+//					vivante = false;
+//				}
+//				if(!vivante && marge) { // si elle est seul mais a de la place pour faire des choses
+//					vivante = true;
+//				}
+						
+				if(vivante){
+					System.out.println(pierre.getCouleur()  + " / " + pierre.getX() + "," + pierre.getY()+ " : vivante");
+				}
+				else {
+					System.out.println(pierre.getCouleur()  + " / " + pierre.getX() + "," + pierre.getY()+ " : morte");
+				}
+						
+				//reset des variable a la fin
+				vivante = false;
+				BordHaut = false;
+				BordBas = false;
+				BordGauche = false;
+				BordDroit = false;
+						
+				listeInterVide.clear();
+				finalInterVide.clear();
+				InterVideDejaParcourue.clear();
+				//---------------------------
 			}
 			
-			if(!vivante) {
-				//afficher une croix au dessus
+			if(!vivante) {  //afficher une croix au dessus
 				if(pierre.hasChaine()) {
 					for(AbstractPierre ap : hmChaine.get(pierre.getNomChaine()).getChaine()) {
 						
